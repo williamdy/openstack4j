0fe40984f2f231ce57d3258c851784b0f4e30782
wangwb <wangwb@svmsoft.com.cn>
2018-09-29 09:40:17 +0800

    svd tmp

--- core/src/main/java/org/openstack4j/api/placement/ResourceProviderService.java
+++ core/src/main/java/org/openstack4j/api/placement/ResourceProviderService.java
@@ -3,7 +3,9 @@ package org.openstack4j.api.placement;
 import java.util.List;
 
 import org.openstack4j.common.RestService;
+import org.openstack4j.model.common.ActionResponse;
 import org.openstack4j.model.palcement.ResourceProvider;
+import org.openstack4j.openstack.placement.domain.PlacementResourceProvider;
 
 public interface ResourceProviderService  extends RestService {
 
@@ -13,5 +15,28 @@ public interface ResourceProviderService  extends RestService {
      * @return list of ResourceProviders or empty
      */
      List<? extends ResourceProvider> list();
+
+     /**
+      * get PlacementResourceProvider by id
+      * 
+      * @param resourceProviderId
+      * @return PlacementResourceProvider by id
+      */
+     PlacementResourceProvider get(String resourceProviderId);
+
+     /**
+      * 
+      * @param resourceProviderId
+      * @return
+      */
+	 ActionResponse delete(String resourceProviderId);
+	 
+	 /**
+	  * 
+	  * @param resourceProvider
+	  * @return
+	  */
+   	 PlacementResourceProvider update(PlacementResourceProvider resourceProvider);
+	 
 
 }
--- core/src/main/java/org/openstack4j/openstack/placement/internal/ResourceProviderServiceImpl.java
+++ core/src/main/java/org/openstack4j/openstack/placement/internal/ResourceProviderServiceImpl.java
@@ -3,9 +3,16 @@ package org.openstack4j.openstack.placement.internal;
 import java.util.List;
 
 import org.openstack4j.api.placement.ResourceProviderService;
+import org.openstack4j.model.common.ActionResponse;
+import org.openstack4j.model.image.Image;
 import org.openstack4j.model.palcement.ResourceProvider;
-import org.openstack4j.openstack.internal.BaseOpenStackService;
+import org.openstack4j.openstack.image.domain.GlanceImage;
+import org.openstack4j.openstack.image.domain.functions.ImageForUpdateToHeaders;
+import org.openstack4j.openstack.octavia.domain.OctaviaLoadBalancerV2;
+import org.openstack4j.openstack.placement.domain.PlacementResourceProvider;
 import org.openstack4j.openstack.placement.domain.PlacementResourceProvider.ResourceProviders;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class ResourceProviderServiceImpl  extends BasePlacementServices implements ResourceProviderService {
 
@@ -13,5 +20,31 @@ public class ResourceProviderServiceImpl  extends BasePlacementServices implemen
 	public List<? extends ResourceProvider> list() {
 		return get(ResourceProviders.class, uri("/resource_providers")).execute().getList();
 	}
+	
+	@Override
+	public PlacementResourceProvider get(String resourceProviderId) {
+		checkNotNull(resourceProviderId);
+		return get(PlacementResourceProvider.class, uri("/resource_providers/%s",resourceProviderId)).param("format", "json").execute();
+	}
 
+	@Override
+	public ActionResponse delete(String resourceProviderId) {
+		checkNotNull(resourceProviderId);
+		return deleteWithResponse(
+				uri("/resource_providers/%s", resourceProviderId)).param(
+				"format", "json").execute();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public PlacementResourceProvider update(
+			PlacementResourceProvider resourceProvider) {
+		checkNotNull(resourceProvider);
+		checkNotNull(resourceProvider.getUuid());
+		
+	    return put(PlacementResourceProvider.class, uri("/resource_providers/%s",resourceProvider.getUuid())).entity(resourceProvider).execute();
+	   
+	}
 }
